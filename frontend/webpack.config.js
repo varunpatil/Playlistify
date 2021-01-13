1968eb0b182706bdebf36ffc71a33c0f2e021da9
Varun Patil <varun99patil@gmail.com>
2021-01-13 21:32:24 +0530

    config changes

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
-    filename: "[name].js",
+    filename: "main.js",
   },
   module: {
     rules: [
