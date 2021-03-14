277d52d2d1ba0776f839b0fb86bd70bba2ce3192
Varun Patil <varun99patil@gmail.com>
2021-03-15 05:24:15 +0530

    friend rec. page UI

--- frontend/src/components/Recommendations/Friend.js
+++ frontend/src/components/Recommendations/Friend.js
@@ -1,10 +1,59 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Box, Button, Paper, Typography, TextField } from "@material-ui/core";
 
 export default function RecFriend() {
+  const [input, setInput] = useState("");
+
+  const submit = async () => {
+    try {
+      const res = await axios.post("/api/recommendation/friend/", {
+        user_id: input,
+      });
+
+      const playlist_id = res.data.playlist_id;
+      console.log(playlist_id);
+    } catch (error) {
+      console.log(error.response.data.Error);
+    }
+  };
+
   return (
-    <Typography variant="title" component="h1">
-      Friend Recomendations
-    </Typography>
+    <div align="center">
+      <Paper style={{ padding: 20 }}>
+        <Typography variant="title" component="h1" align="center">
+          Enter link to your friend's profile
+        </Typography>
+
+        <Typography align="center">
+          Make sure the link resembles{" "}
+          <code>https://open.spotify.com/user/xyz</code> or{"  "}
+          <code>spotify:user:xyz</code>
+        </Typography>
+      </Paper>
+
+      <Box m={4} />
+
+      <TextField
+        placeholder="https://open.spotify.com/user/xyz"
+        fullWidth
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+
+      <Box m={4} />
+
+      <Button
+        variant="contained"
+        size="medium"
+        color="primary"
+        style={{ borderRadius: 25 }}
+        onClick={submit}
+      >
+        <strong>Create recommendation playlist</strong>
+      </Button>
+    </div>
   );
 }
